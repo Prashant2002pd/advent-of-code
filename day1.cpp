5cfc3fd88a6b59e8b9527bfa54eda4629aef5c42
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2023-12-06 17:29:25 +0530

    first commut

--- day1.cpp
+++ day1.cpp
@@ -10,6 +10,7 @@ int main(){
   ifstream f("input.txt");
 
   while(getline(f,mytext)){
+    //this loop will get the first no.in the string
     for(int i=0;i<mytext.size();i++){
       if(isdigit(mytext[i])){
 	num=int((mytext[i])-48)*10;
@@ -17,6 +18,7 @@ int main(){
 	break;
       }
     }
+    //this loop will get the second no. in the string add the first no. to it to make it a full to digit no.
     for(int i=mytext.size()-1;i>=0;i--){
       if(isdigit(mytext[i])){
 	num+=int(mytext[i])-48;
@@ -24,7 +26,7 @@ int main(){
 	break;
       }
     }
-
+    //then just add the num to the final sum
     sum+=num;
   }
   cout<<sum;
